97146552542ff808b9680881e75eddc69e21ccef
Vinicius Maximo <vinicius.sm6@gmail.com>
2021-04-28 20:01:52 -0300

    crud inexigibilidade

---
+++ public/inexigibilidade/editInexigibilidade.php
@@ -0,0 +1,282 @@
+<?php include("../Template/header.php");
+include("../../data/connection.php");
+
+if (isset ($_GET["id"])) {
+    $id = $_GET["id"];
+    
+    $sql = "SELECT * FROM inexigibilidade WHERE id_inexigibilidade = '". $id . "'";
+    $resultado = $connection->query($sql);
+    $inexigibilidade = $resultado->fetch_assoc();
+
+    
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inexigibilidade</title>
+</head>
+
+<body>
+    <br>
+    <br>
+
+    
+    <div class="form">
+        <form action="updateInexigibilidade.php" method="POST" style="margin-left: 100px; margin-right: 100px;">
+            <h3>Edit Inexigibilidade</h3>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Id da inexigibilidade</span>
+                </div>
+
+                <input type="number" name="inexigibilidadeId" class="form-control input-sm" id="inexgibilidadeId" aria-label="Default" aria-describedby="inputGroup-sizing-default" value="<?php echo $inexigibilidade["id_inexigibilidade"]?>" readonly>
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Exercício</span>
+                </div>
+                <input type="text" name="txtExercicio" class="form-control" id="txtExercicio" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Nº Processo</span>
+                </div>
+                <input type="number" name="numProcesso" class="form-control" id="numProcesso" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Nº Inexigibilidade</span>
+                </div>
+                <input type="number" name="numInexigibilidade" class="form-control" id="numInexigibilidade" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Objeto</span>
+                </div>
+                <input type="text" name="txtObjeto" class="form-control" id="txtObjeto" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Categoria</span>
+                </div>
+                <select class="form-select" name="txtCategoria" id="txtCategoria">
+                    <option value="-1" selected>Selecione a categoria</option>
+                    <?php
+
+                    $sqlQuery = "SELECT * FROM categoria ORDER BY categoria";
+
+                    $categoria = $connection->query($sqlQuery);
+
+                    if ($categoria->num_rows > 0) {
+
+                        while ($row = $categoria->fetch_assoc()) {
+                    ?>
+
+                            <option value="<?php echo $row["categoria"] ?>">
+                                <?php echo $row["categoria"]  ?>
+                            </option>
+
+                    <?php
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+
+
+            
+            
+           
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data de início</span>
+                </div>
+                <input type="date" name="datei" class="form-control" id="datei" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Portaria nomeando a comissão</span>
+                </div>
+                <input type="text" name="txtp" class="form-control" id="txtp" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data da solicitação de compras/serviços</span>
+                </div>
+                <input type="date" name="dates" class="form-control" id="dates" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data de preço estimativo (Orçamento)</span>
+                </div>
+                <input type="date" name="datepe" class="form-control" id="datepe" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data da autorização para a abertura do processo</span>
+                </div>
+                <input type="date" name="datea" class="form-control" id="datea" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data autuação</span>
+                </div>
+                <input type="date" name="dateau" class="form-control" id="dateau" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Especificação do projeto</span>
+                </div>
+                <input type="text" name="txtEspecificacao" class="form-control" id="txtEspecificacao" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Enquadramento na modalidade pertinente</span>
+                </div>
+                <input type="text" name="txtEnquadramento" class="form-control" id="txtEnquadramento" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Minuta do contrato</span>
+                </div>
+                <input type="text" name="txtMinuta" class="form-control" id="txtMinuta" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data aprovação da minuta</span>
+                </div>
+                <input type="date" name="dateAprov" class="form-control" id="dateAprov" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data proposta do favorecido</span>
+                </div>
+                <input type="date" name="dateProp" class="form-control" id="dateProp" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Documentos de habilitação</span>
+                </div>
+                <input type="text" name="txtHabilitacao" class="form-control" id="txtHabilitacao" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data ata de inexigibilidade</span>
+                </div>
+                <input type="date" name="dateAta" class="form-control" id="dateAta" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data parecer jurídico</span>
+                </div>
+                <input type="date" name="dateParecer" class="form-control" id="dateParecer" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data ratificação</span>
+                </div>
+                <input type="date" name="dateRatificacao" class="form-control" id="dateRatificacao" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data contrato firmado</span>
+                </div>
+                <input type="date" name="dateCF" class="form-control" id="dateCF" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data publicação da inexigibilidade</span>
+                </div>
+                <input type="date" name="datePublicacao" class="form-control" id="datePublicacao" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Cópia notas de empenho e compr. legais</span>
+                </div>
+                <input type="text" name="txtCopias" class="form-control" id="txtCopias" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Código de registro</span>
+                </div>
+                <input type="text" name="txtCodReg" class="form-control" id="txtCodReg" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Valor total do processo</span>
+                </div>
+                <input type="number" name="numTotal" class="form-control" id="numTotal" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Observações</span>
+                </div>
+                <input type="text" name="txtObservacoes" class="form-control" id="txtObservacoes" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Data e hora de lançamento</span>
+                </div>
+                <input type="datetime-local" name="dateLancamento" class="form-control" id="dateLancamento" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <br>    
+            <div class="buttons">
+                
+                <input type="submit" class="btn btn-success" value="Cadastrar">
+                <input type="reset" class="btn btn-danger"  onclick="window.location.href='../login/index.php'" value="Cancelar">
+
+            </div>
+
+            
+
+            
+
+
+            
+        </form>
+    </div>
+
+
+</body>
+</html>
+
+<?php
+}
+
+?>
---
+++ public/inexigibilidade/listInexigibilidadeNoSearch.php
@@ -0,0 +1,76 @@
+<?php include("../Template/header.php") ?>
+
+<?php
+$usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
+?>
+ 
+
+<?php
+
+
+
+    include("../../data/connection.php");
+
+    $sql = "SELECT * FROM inexigibilidade";
+
+
+   
+
+
+    $dadosInexigibilidade = $connection -> query($sql);
+
+    if($dadosInexigibilidade -> num_rows > 0)
+    {
+    ?>
+    <div class="form">
+
+    
+
+    
+        <div style="margin-left: 100px; margin-right: 100px;" style="text-align: center;">
+        <br>
+       
+            <br>
+            <table class="table" style="text-align: center;">
+                <tr>
+                    <th>Exercício</th>
+                    <th>Número do processo</th>
+                    <th>Número da inexigibilidade</th>
+                    <th></th>
+                </tr>
+
+                <?php
+                    while($exibir = $dadosInexigibilidade -> fetch_assoc())
+                    {
+                    ?>
+                        <tr>
+                        <td><?php echo $exibir["exercicio"] ?></td>
+                        <td><?php echo $exibir["num_processo"] ?></td>
+                        <td><?php echo $exibir["num_inexigibilidade"] ?></td>
+                            <td>
+                                <button type="submit" class="btn btn-danger btn-sm" formmethod="post">
+                                    <a href="deleteInexigibilidade.php?id=<?php echo $exibir ["id_inexigibilidade"] ?>" style="text-decoration: none; color: white"> Excluir </a> 
+                                    
+                                </button>
+                                <button type="submit" class="btn btn-success" formmethod="post">
+                                <a href="editInexigibilidade.php?id=<?php echo $exibir["id_inexigibilidade"] ?>" style="text-decoration: none; color: white">Editar</a>
+                                </button> 
+                            </td>
+                        </tr>
+                    <?php
+                        }
+                    ?>
+            </table>
+        </div>
+    </div>
+    <?php
+    }
+    else
+    {
+        echo "Nenhum registro encontrado.";
+    }
+    ?>
+
+<?php
+error_reporting(0);
+?>
