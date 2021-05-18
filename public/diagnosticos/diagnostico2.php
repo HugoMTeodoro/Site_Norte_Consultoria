6db83566b6157d0cba410df25adf7fca02c13343
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-18 20:47:46 -0300

    diagnosticos

---
+++ public/diagnosticos/diagnostico2.php
@@ -0,0 +1,67 @@
+<?php
+include("../../data/connection.php");
+
+$mediadisp=0;
+$mediaadesao=0;
+$mediainex=0;
+$mediamodal=0;
+$mediapregao=0;
+
+$sql="select SUM(datediff(data_pesquisa, data_solicitacao_compras_servicos)) as mediadispensa, count(*) as quantdisp from dispensa";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    
+    while ($exibir = $dados->fetch_assoc()) {
+        if($exibir["quantdisp"]>0){
+        $mediadisp=$exibir["mediadispensa"]/$exibir["quantdisp"];
+        }
+    }
+
+}
+echo $mediadisp;
+
+$sql="select SUM(datediff(data_pesquisa, dt_solicitacao)) as mediaadesao, count(*) as quantadesao from adesao";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        if($exibir["quantadesao"]>0){
+        $mediaadesao=$exibir["mediaadesao"]/$exibir["quantadesao"];
+        }
+    }
+}
+echo $mediaadesao;
+
+
+$sql="select SUM(datediff(data_pesquisa, dt_solicitacao)) as mediainex, count(*) as quantinex from inexigibilidade";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        if($exibir["quantinex"]>0){
+        $mediainex=$exibir["mediainex"]/$exibir["quantinex"];
+        }
+    }
+}
+echo $mediainex;
+
+$sql="select SUM(datediff(data_pesquisa, dt_solicitacao)) as mediamodal, count(*) as quantmod from modalidade";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        if($exibir["quantmod"]>0){
+        $mediamodal=$exibir["mediamodal"]/$exibir["quantmod"];
+        }
+    }
+}
+echo $mediamodal;
+
+$sql="select SUM(datediff(data_pesquisa, dt_solicitacao)) as mediapregao, count(*) as quantpregao from pregao";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        if($exibir["quantpregao"]>0){
+        $mediapregao=$exibir["mediapregao"]/$exibir["quantpregao"];
+        }
+    }
+}
+echo $mediapregao;
+?>
---
+++ public/diagnosticos/diagnostico5.php
@@ -0,0 +1,36 @@
+<?php
+include("../../data/connection.php");
+
+$quantdisp=0;
+$quantadesao=0;
+$quantinex=0;
+$quantmodal=0;
+$quantpregao=0;
+
+
+$sql="select categoria from categoria";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    
+    while ($exibir = $dados->fetch_assoc()) {
+        $cat=$exibir["categoria"];
+        $sql2="SELECT count(*) as quantdisp FROM `dispensa` where categoria='$cat'";
+        $dados2 = $connection->query($sql2);
+        if ($dados2->num_rows > 0) {
+            while ($exibir2 = $dados2->fetch_assoc()) {
+            $quantdisp=$exibir2["quantdisp"];
+            }
+        }
+
+        $sql3="SELECT count(*) as quantadesao FROM `adesao` where categoria='$cat'";
+        $dados3 = $connection->query($sql2);
+        if ($dados3->num_rows > 0) {
+            while ($exibir3 = $dados3->fetch_assoc()) {
+            $quantadesao=$exibir3["quantdisp"];
+            }
+        }
+
+
+    }
+
+}
