2e6288285bb859dc317654e8db0587a58841b982
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-18 16:44:24 -0300

    parei no diagostico2

--- public/diagnosticos/diagnostico1.php
+++ public/diagnosticos/diagnostico1.php
@@ -1,18 +1,56 @@
 <?php include("../Template/header.php");
 include("../../data/connection.php");
 
-$sql="select SUM(datediff(data_ratificacao, data_solicitacao_compras_servicos)) as mediadispensa, count(*) as quantpregao from dispensa";
+$mediadisp;
+$mediaadesao;
+$mediainex;
+$mediamodal;
+$mediapregao;
+
+$sql="select SUM(datediff(data_ratificacao, data_solicitacao_compras_servicos)) as mediadispensa, count(*) as quantdisp from dispensa";
 $dados = $connection->query($sql);
 if ($dados->num_rows > 0) {
     while ($exibir = $dados->fetch_assoc()) {
-        
+        $mediadisp=$exibir["mediadispensa"]/$exibir["quantdisp"];
     }
+}
+echo $mediadisp;
 
-$sql="select SUM(datediff(dt_ratificacao, dt_solicitacao)) as mediaadesao, count(*) as quantpregao from adesao";
-$sql="select SUM(datediff(dt_ratificacao, dt_solicitacao)) as mediainex, count(*) as quantpregao from inexigibilidade";
-$sql="select SUM(datediff(dt_homologacao, dt_solicitacao)) as mediamodal, count(*) as quantpregao from modalidade";
-$sql="select SUM(datediff(dt_homologacao, dt_solicitacao)) as mediamodal, count(*) as quantpregao from pregao";
+$sql="select SUM(datediff(dt_ratificacao, dt_solicitacao)) as mediaadesao, count(*) as quantadesao from adesao";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        $mediaadesao=$exibir["mediaadesao"]/$exibir["quantadesao"];
+    }
+}
+echo $mediaadesao;
 
 
+$sql="select SUM(datediff(dt_ratificacao, dt_solicitacao)) as mediainex, count(*) as quantinex from inexigibilidade";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        $mediainex=$exibir["mediainex"]/$exibir["quantinex"];
+    }
+}
+echo $mediainex;
+
+$sql="select SUM(datediff(dt_homologacao, dt_solicitacao)) as mediamodal, count(*) as quantmod from modalidade";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        $mediamodal=$exibir["mediamodal"]/$exibir["quantmod"];
+    }
+}
+echo $mediamodal;
+
+$sql="select SUM(datediff(dt_homologacao, dt_solicitacao)) as mediapregao, count(*) as quantpregao from pregao";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        $mediapregao=$exibir["mediapregao"]/$exibir["quantpregao"];
+    }
+}
+echo $mediapregao;
 
 ?>
