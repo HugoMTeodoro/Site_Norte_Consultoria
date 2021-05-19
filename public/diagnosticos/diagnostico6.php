de1cd8c44bf0c98818937437dffebbdec27d975d
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-18 21:22:33 -0300

    diagnosticos  gerais semi prontos

---
+++ public/diagnosticos/diagnostico6.php
@@ -0,0 +1,16 @@
+<?php
+include("../../data/connection.php");
+
+$quantdisp = 0;
+
+
+
+$sql = "select count(*) as quantdisp from dispensa where emergencia='sim'";
+$dados = $connection->query($sql);
+if ($dados->num_rows > 0) {
+    while ($exibir = $dados->fetch_assoc()) {
+        $quantdisp=$exibir["quantdisp"];
+    }
+}
+echo $quantdisp;
+?>
