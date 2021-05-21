49342db55b5563a58b186cd4a7879c2428857de7
Vinicius Maximo <vinicius.sm6@gmail.com>
2021-05-21 19:54:20 -0300

    inex

--- public/inexigibilidade/listInexigibilidade.php
+++ public/inexigibilidade/listInexigibilidade.php
@@ -3,6 +3,12 @@
 
 <?php include("../../data/connection.php") ?>
 
+<script>
+	function redirectPage(v) {
+		document.location.href = '../orgao/exercicioAtuali.php?param= '+v;  
+		
+	}
+</script>
 <br>
 <br>
 <br>
@@ -12,6 +18,37 @@
     <div style="margin-left: 100px; margin-right: 100px;">
         <h2>Inexigibilidade</h2>
 
+		<div class="input-group mb-3">
+			<div class="input-group-prepend">
+				<span class="input-group-text" id="inputGroup-sizing-default">Exercício</span>
+			</div>
+			<select class="form-select" name="txtOrgao" id="txtOrgao" onChange="redirectPage(this.value)">
+				<option value="<?php echo "Selecione" ?>" selected>
+					<?php echo "Selecione" ?>
+				</option>
+				<?php
+
+				$sqlQuery = "SELECT DISTINCT exercicio FROM inexigibilidade order by exercicio";
+
+				$orgaos = $connection->query($sqlQuery);
+
+				if ($orgaos->num_rows > 0) {
+
+					while ($row = $orgaos->fetch_assoc()) { {
+				?>
+
+							<option value="<?php echo $row["exercicio"] ?>">
+								<?php echo $row["exercicio"] ?>
+							</option>
+
+				<?php
+						}
+					}
+				}
+
+				?>
+			</select>
+		</div>
         <br>
         
 		<form action="createInexigibilidade.php" method="POST">
---
+++ public/orgao/exercicioAtuali.php
@@ -0,0 +1,14 @@
+<?php
+include_once("../../data/connection.php");
+$exercicio = $_GET["param"];
+$sql = "UPDATE  
+    orgao_atual set exercicio_atual='$exercicio' where id_orgao_atual=1";
+    $resultado = $connection -> query($sql);
+?>
+<script>
+url = new URL(window.location.href);
+url.searchParams.delete("param");
+window.location.href=('Object', 'Title', '/projetos/Site_Norte_Consultoria/public/inexigibilidade/listInexigibilidade.php');
+
+
+</script>
