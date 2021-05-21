7d6eecedfd0e52a2c786e0519ddb840191ea2405
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-21 19:08:07 -0300

    searching dispensa

--- public/dispensa/listDispensaSearching.php
+++ public/dispensa/listDispensaSearching.php
@@ -13,7 +13,7 @@ $usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
 
     include("../../data/connection.php");
 
-    $sql = "SELECT * FROM dispensa WHERE exercicio = $exer and orgao ='$orgaoa' and (pesquisa LIKE '%$usuarios%' or objeto LIKE '%$usuarios%') ";
+    $sql = "SELECT * FROM dispensa WHERE exercicio = $exer and orgao ='$orgaoa' and (pesquisa LIKE '%$usuarios%' or objeto LIKE '%$usuarios%') order by num_dispensa ";
 
     $dadosDispensa = $connection -> query($sql);
     
@@ -47,14 +47,21 @@ $usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
                         <tr>
                         <td><?php echo $exibir["pesquisa"] ?></td>
                         <td><?php echo $exibir["objeto"] ?></td>
-                            <td>
+                        <td>
                                 <button type="submit" class="btn btn-danger btn-sm" formmethod="post">
                                     <a href="deleteDispensa.php?id=<?php echo $exibir ["id_dispensa"] ?>" style="text-decoration: none; color: white"> Excluir </a> 
-                                    
                                 </button>
-                                <button type="submit" class="btn btn-success" formmethod="post">
-                                <a href="editDispensa.php?id=<?php echo $exibir["id_dispensa"] ?>" style="text-decoration: none; color: white">Editar</a>
-                                </button> 
+                                
+                                <form name="editbutton" action="editDispensa.php" method="POST">
+                                <input type="hidden" name="codigo" value="<?php echo $exibir["codigo_processo"] ?>">
+                                <input type="hidden" name="action" value="../dispensa/editDispensa.php">
+                                
+                                <br>
+                                <input type="hidden" name="tipo" value="dispensa">
+                                <input type="hidden" name="id" value="<?php echo $exibir["id_dispensa"] ?>">
+                                
+                                <input type="submit" class="btn btn-success btn-sm" value="Editar">
+                            </form>
                             </td>
                         </tr>
                     <?php
