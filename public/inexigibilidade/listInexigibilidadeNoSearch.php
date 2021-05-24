08fb8c9d61d98f5fd3fc05b38e9d1781f8c16309
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-23 21:33:18 -0300

    txt inex gerado

--- public/inexigibilidade/listInexigibilidadeNoSearch.php
+++ public/inexigibilidade/listInexigibilidadeNoSearch.php
@@ -3,33 +3,32 @@
 <?php
 $usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
 ?>
- 
+
 
 <?php
 
 
 
-    include("../../data/connection.php");
+include("../../data/connection.php");
 
-    $sql = "SELECT * FROM inexigibilidade";
-
-
-   
+$sql = "SELECT * FROM inexigibilidade";
 
 
-    $dadosInexigibilidade = $connection -> query($sql);
 
-    if($dadosInexigibilidade -> num_rows > 0)
-    {
-    ?>
+
+
+$dadosInexigibilidade = $connection->query($sql);
+
+if ($dadosInexigibilidade->num_rows > 0) {
+?>
     <div class="form">
 
-    
 
-    
+
+
         <div style="margin-left: 100px; margin-right: 100px;" style="text-align: center;">
-        <br>
-       
+            <br>
+
             <br>
             <table class="table" style="text-align: center;">
                 <tr>
@@ -40,19 +39,21 @@ $usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
                 </tr>
 
                 <?php
-                    while($exibir = $dadosInexigibilidade -> fetch_assoc())
-                    {
-                    ?>
-                        <tr>
+                while ($exibir = $dadosInexigibilidade->fetch_assoc()) {
+                ?>
+                    <tr>
                         <td><?php echo $exibir["exercicio"] ?></td>
                         <td><?php echo $exibir["num_processo"] ?></td>
                         <td><?php echo $exibir["num_inexigibilidade"] ?></td>
-                            <td>
-                                <button type="submit" class="btn btn-danger btn-sm" formmethod="post">
-                                    <a href="deleteInexigibilidade.php?id=<?php echo $exibir ["id_inexigibilidade"] ?>" style="text-decoration: none; color: white"> Excluir </a> 
-                                </button>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm">
+                                <a href="../geracaoDeDocx/coletaInex.php?id=<?php echo $exibir["codigo_processo"] ?>" style="text-decoration: none; color: white" target="blank">Relatorio</a>
+                            </button>
+                            <button type="submit" class="btn btn-danger btn-sm" formmethod="post">
+                                <a href="deleteInexigibilidade.php?id=<?php echo $exibir["id_inexigibilidade"] ?>" style="text-decoration: none; color: white"> Excluir </a>
+                            </button>
 
-                                <form name="editbutton" action="editInexigibilidade.php" method="POST">
+                            <form name="editbutton" action="editInexigibilidade.php" method="POST">
                                 <input type="hidden" name="codigo" value="<?php echo $exibir["codigo_processo"] ?>">
                                 <input type="hidden" name="action" value="../inexigibilidade/editInexigibilidade.php">
 
@@ -61,21 +62,19 @@ $usuarios = filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
                                 <input type="hidden" name="id" value="<?php echo $exibir["id_inexigibilidade"] ?>">
                                 <input type="submit" class="btn btn-success btn-sm" value="Editar">
                             </form>
-                            </td>
-                        </tr>
-                    <?php
-                        }
-                    ?>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </table>
         </div>
     </div>
-    <?php
-    }
-    else
-    {
-        echo "Nenhum registro encontrado.";
-    }
-    ?>
+<?php
+} else {
+    echo "Nenhum registro encontrado.";
+}
+?>
 
 <?php
 error_reporting(0);
