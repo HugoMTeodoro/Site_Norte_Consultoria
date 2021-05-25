8aa637b8fe6875de6074164c3f703cca1c5e0ea8
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-05-25 18:15:12 -0300

    commitando

--- public/inexigibilidade/deleteInexigibilidade.php
+++ public/inexigibilidade/deleteInexigibilidade.php
@@ -1,26 +1,50 @@
-<?php 
-        include("../Login/valida.php");
-        include_once("../../data/connection.php");
-        if (isset ($_GET["id"])) {
-            $sql = "DELETE FROM inexigibilidade WHERE id_inexigibilidade = " . $_GET["id"];
+<?php
+include("../Login/valida.php");
+include_once("../../data/connection.php");
+if (isset($_GET["id"])) {
 
-            if ($connection -> query($sql) === TRUE) {
- ?>
-                <script>
-            alert ("Inexigibilidade excluída com sucesso");
+
+    $sql = "select codigo_processo FROM inexigibilidade WHERE id_inexigibilidade = " . $_GET["id"];
+    $resultado = $connection->query($sql);
+
+    if ($resultado->num_rows > 0) {
+        while ($exibir = $resultado->fetch_assoc()) {
+            $codigo = $exibir["codigo_processo"];
+        }
+    }
+
+    $sql = "DELETE FROM vencedores  WHERE id_inexigibilidade ='$codigo' ";
+    $resultado = $connection->query($sql);
+
+    $sql = "DELETE FROM aditivo  WHERE id_inexigibilidade = '$codigo'";
+    $resultado = $connection->query($sql);
+
+
+
+
+
+
+
+
+
+    $sql = "DELETE FROM inexigibilidade WHERE id_inexigibilidade = " . $_GET["id"];
+
+    if ($connection->query($sql) === TRUE) {
+?>
+        <script>
+            alert("Inexigibilidade excluída com sucesso");
             window.location = "listInexigibilidade.php";
-                </script>
+        </script>
 
-                <?php
-            
+    <?php
 
-            }
-        else {
-               ?>
-            <script>
+
+    } else {
+    ?>
+        <script>
             alert("Erro ao excluir a inexigibilidade");
             window.location = "listInexigibilidade.php";
-            </script> 
+        </script>
 <?php
 
     }
